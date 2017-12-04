4a7acf395a611593a623cc523a6e527fa5ca7a8e
Peeter Roop <petsens@users.noreply.github.com>
2017-12-04 13:49:22 +0200

    sisselogimine, Petsi asjad

---
+++ login-pets.php
@@ -0,0 +1,36 @@
+<?php
+	require("style.css");
+	
+	
+?>
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Flat HTML5/CSS3 Login Form</title>
+  
+  
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+  <div class="login-page">
+  <div class="form">
+    <form class="login-form">
+      <input type="text" placeholder="Kasutajanimi"/>
+      <input type="password" placeholder="Parool"/>
+      <button><a href="chatselect.php">
+	  Logi sisse</a></button>
+    </form>
+  </div>
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script  src="js/index.js"></script>
+
+</body>
+</html>
