0c2731d9ce17981c9bd9505298d86574f4dd2562
Robin Nurk <robinnurk@users.noreply.github.com>
2017-12-04 14:28:33 +0200

    õiged

--- functions.php
+++ functions.php
@@ -31,3 +31,181 @@ function listMsg(){
 	return $notice;
 }
 ?>
+
+<?php
+	$database = "if17_oskarj";
+	require("../../../config.php");
+	
+	session_start();
+	
+	function signIn($email, $password){
+		$notice = "";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("SELECT id, firstname, lastname, email, password FROM chatusers WHERE email = ?");
+		$stmt->bind_param("s", $email);
+		$stmt->bind_result($id, $firstnameFromDb, $lastnameFromDb, $emailFromDb, $passwordFromDb);
+		$stmt->execute();
+		
+		//kontrollime vastavust
+		if ($stmt->fetch()){
+			$hash = hash("sha512", $password);
+			if ($hash == $passwordFromDb){
+				$notice = "Logisite sisse!";
+				
+				//Määran sessiooni muutujad
+				$_SESSION["userId"] = $id;
+				$_SESSION["firstname"] = $firstnameFromDb;
+				$_SESSION["lastname"] = $lastnameFromDb;
+				$_SESSION["userEmail"] = $emailFromDb;
+				
+				//liigume pealehele
+				header("Location: main.php");
+				exit();
+			} else {
+				$notice = "Sisestasite vale salasõna!";
+			}
+		} else {
+			$notice = "Sellist kasutajat (" .$email .") ei ole!";
+		}
+		return $notice;
+	}
+	
+	//uue kasutaja andmebaasi lisamine
+	function signUp($signupFirstName, $signupFamilyName, $signupEmail, $signupPassword){
+		//loome andmebaasiühenduse
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		//valmistame ette käsu andmebaasiserverile
+		$stmt = $mysqli->prepare("INSERT INTO chatusers (firstname, lastname, email, password) VALUES (?, ?, ?, ?)");
+		echo $mysqli->error;
+		//s - string
+		//i - integer
+		//d - decimal
+		$stmt->bind_param("sssiss", $signupFirstName, $signupFamilyName, $signupEmail, $signupPassword);
+		//$stmt->execute();
+		if ($stmt->execute()){
+			echo "\n Õnnestus!";
+		} else {
+			echo "\n Tekkis viga : " .$stmt->error;
+		}
+		$stmt->close();
+		$mysqli->close();
+	}
+	
+?>
+
+
+
+<?php
+//Et pääseks ligi funktsioonidele ja sessionile
+require("functions.php");
+$notice = "";
+require("../../../config.php");
+	
+if(isset($_POST["saveMessage"])){
+	
+	if(isset($_POST["sonum"]) and !empty($_POST["sonum"])){
+		$notice = saveMsg($_POST["sonum"]);
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+</head>
+
+
+
+<body>
+	<h2>Chati nimi</h2>
+	<div style="chatbox">
+		<div class="chatbox">
+		<?php echo listMsg(); ?>
+		</div>
+	</div>
+	
+	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+		<label>Text: </label>
+		<input name="sonum" type="text">
+		<input name="saveMessage" type="submit" value="Saada!">
+		<span><?php echo $notice; ?></span>
+		
+	</form>
+	
+	
+</body>
+</html>
+<?php
+	$database = "if17_nurkrobi";
+	$serverHost = "localhost";
+	$serverUsername = "if17";
+	$serverPassword = "if17";
+	
+	$name = "robin";
+	function saveMsg($text, $name){
+		$notice = "";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("INSERT INTO chat (text, name) VALUES (?, ?)");
+		echo $mysqli->error;
+		$stmt->bind_param("ss", $text, $name);
+		if($stmt->execute()){
+			$notice = "Saadetud!";
+		} else {
+			$notice = "Saatmisel tekkis viga: " .$stmt->error;
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		return $notice;
+	}
+
+
+	if(isset($_POST["msgButton"])){
+		if(isset($_POST["text"]) and !empty($_POST["text"])){
+			//echo $_POST["ideaColor"];
+			$notice = saveMsg($_POST["text"], $name);
+			
+		}
+	}
+
+	function showMsg(){
+		$notice = "";
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("SELECT text, name, time FROM chat ORDER BY time DESC");
+		echo $mysqli->error;
+		$stmt->bind_result($text, $name, $time);
+		$stmt->execute();
+		
+		while($stmt->fetch()){
+			
+			$notice .= "Saadetud- ".$time ."  ".$name ." : " .$text ." \n";
+			//$notice .= '<p style="background-color: ' .$color .'">' .$idea ."</p> \n";
+			//<p style="background-color: #ffff80">Hea mõte</p>
+			//<p style="background-color: #ffff80">Hea mõte | <a href ="edituseridea.php?id=13">Toimeta</a></p>
+		}
+	
+		$stmt->close();
+		$mysqli->close();
+		return $notice;
+	}
+?>
+
+<html>
+	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+		<label>Saada sõnum: </label>
+		<input name="text" type="text">
+		<br>
+		<br>
+		<input name="msgButton" type="submit" value="Saada sõnum!">
+		
+	</form>
+	<hr>
+	<div style="width: 40%">
+		<?php echo showMsg(); ?>
+	</div>
+</html>
